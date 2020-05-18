7116855bd2026c877aadaf111cb33014c218c709
michaldobiezynski <dobiezynski@gmail.com>
2020-05-18 15:25:31 +0100

    components organised and styled

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+    <div class="row">
+        <div class="col-xs-12">
+            <header>
+                <h1>Server Status</h1>
+            </header>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
 </script>
 
-<style scoped></style>
+<style>
+
+</style>
