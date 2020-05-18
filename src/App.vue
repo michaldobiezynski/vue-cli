136d497564bfc0f0194b5f7550cf1c64a6d53fbc
michaldobiezynski <dobiezynski@gmail.com>
2020-05-18 15:16:51 +0100

    moving to a better folder structure

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <app-servers></app-servers>
 </template>
 
-<script></script>
+<script>
+import Header from "./components/Shared/Header";
+import Footer from "./components/Shared/Footer";
+import Servers from "./components/Server/Servers";
+import ServerDetails from "./components/Server/ServerDetails";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-servers": Servers,
+    "app-server-etails": ServerDetails
+  }
+};
+</script>
 
 <style></style>
